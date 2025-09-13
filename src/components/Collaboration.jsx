bc97cefaff46768970cb1a3d980f6ab3453c1429
Just A Guy <34491142+justaguyeu@users.noreply.github.com>
2025-09-13 12:21:02 +0300

    FI

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -11,7 +11,7 @@ const Collaboration = () => {
       <div className="container lg:flex">
         <div className="max-w-[25rem]">
           <h2 className="h2 mb-4 md:mb-8">
-            What We Do The Best
+            What We Do Best
           </h2>
 
           <ul className="max-w-[22rem] mb-10 md:mb-14">
