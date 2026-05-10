374ff6c77f370c635f61d20cc8e5dfd9b7b2606b
justaguyou <justaguyeu@gmail.com>
2026-05-10 21:40:02 +0300

    cac

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -146,6 +146,8 @@ const Hero = () => {
           </p> */}
           <p className="body-1 max-w-3xl mx-auto mb-1 text-n-2 lg:mb-3 text-center">Also, we aim to support the Government of Tanzania in addressing the youth employment challenge by providing On-the-job Sales & Marketing Training and Job placement service to equip individuals with necessary sales and marketing skills, enabling them to secure employment and helping business owners find highly skilled salespeople to boost their sales volumes.
           </p>
+          <p className="body-1 max-w-3xl mx-auto mb-1 text-n-2 lg:mb-3 text-center">This is also an online platform which promotes products and services to potential customers, increasing awareness and driving sales. 
+          </p>
         </Section>
       </div>
 
