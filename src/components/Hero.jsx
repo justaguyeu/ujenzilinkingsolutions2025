0e907f0e233173de9988d42c9227ceb404c694a2
justaguyou <justaguyeu@gmail.com>
2026-05-13 11:39:14 +0300

    chana

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,6 @@ const Hero = () => {
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
             We take you to {` `}
-            {/* Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `} */}
             <span className="inline-block relative">
               New Markets{" "}
               <img
@@ -43,10 +42,6 @@ const Hero = () => {
               />
             </span>
           </h1>
-
-          {/* <Button href="/pricing" white>
-            JOIN OUR TEAM
-          </Button> */}
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
@@ -62,52 +57,21 @@ const Hero = () => {
                   alt="Ujenzi Linking Solutions Hero"
                   fetchpriority="high"
                 />
-
-                {/* <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" /> */}
-
-                {/* <ScrollParallax isAbsolutelyPositioned>
-                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
-                    {heroIcons.map((icon, index) => (
-                      <li className="p-5" key={index}>
-                        <img src={icon} width={24} height={25} alt={icon} />
-                      </li>
-                    ))}
-                  </ul>
-                </ScrollParallax> */}
-
-                {/* <ScrollParallax isAbsolutelyPositioned>
-                  <Notification
-                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
-                    title="Developers"
-                  />
-                </ScrollParallax> */}
               </div>
 
             </div>
-
-            {/* <Gradient/> */}
           </div>
-          {/* <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-          <img
-              src={heroBackground}
-              className="w-full"
-              width={1440}
-              height={1800}
-              alt="hero"
-            />
-          </div> */}
 
           <BackgroundCircles />
         </div>
-
-        {/* <CompanyLogos className="hidden relative z-10 mt-2 lg:block" /> */}
         <Section className="overflow-hidden" id="pricing" >
           <Heading
-            className="md:max-w-md lg:max-w-2xl text-center text-color-1" 
+            className="md:max-w-md lg:max-w-2xl text-left text-color-1" 
             title="Abous Us"
           />
+  
           <div className="space-y-6 sm:space-y-6">
-          <p className="body-1 max-w-3xl mx-auto mb-1 text-n-2 lg:mb-3 text-center">
+          <p className="body-1 max-w-3xl mx-auto mb-1 text-n-2 lg:mb-3 text-left">
             Welcome to Ujenzi Linking Solutions where businesses, markets,
             and opportunities connect. Founded in Tanzania in 2024, Ujenzi
             (Kiswahili for Construction) is committed to helping businesses
@@ -115,39 +79,12 @@ const Hero = () => {
             access affordable, tailor-made sales and marketing solutions.
             </p>
           <div className="container md:pb-10">
-            {/* <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
-              {roadmap.map((item) => {
-                const status = item.status === "done" ? "CEO" : "MANAGER";
-
-                return (
-                  <div
-                    className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${item.colorful ? "bg-conic-gradient" : "bg-n-6"
-                      }`}
-                    key={item.id}
-                  >
-                    <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
-                      
-                      <div className="relative z-1">
-                        
-                        <h4 className="h4 mb-4 text-color-1">{item.title}</h4>
-                        <p className="body-2 text-n-4">{item.text}</p>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div> */}
             </div>
 
-          {/* <p className="body-1 max-w-3xl mx-auto mb-1 text-n-2 lg:mb-3 text-center mt-6">Beyond business growth, we are passionate about tackling youth unemployment
-            in Tanzania. Through practical Sales & Marketing Training, on-the-job
-            experience, and job placement services, we equip young people with the
-            skills they need to secure employment while helping companies find skilled
-            and motivated sales professionals to boost performance.
-          </p> */}
-          <p className="body-1 max-w-3xl mx-auto mb-1 text-n-2 lg:mb-3 text-center">Also, we aim to support the Government of Tanzania in addressing the youth employment challenge by providing On-the-job Sales & Marketing Training and Job placement service to equip individuals with necessary sales and marketing skills, enabling them to secure employment and helping business owners find highly skilled salespeople to boost their sales volumes.
+          
+          <p className="body-1 max-w-3xl mx-auto mb-1 text-n-2 lg:mb-3 text-left">Also, we aim to support the Government of Tanzania in addressing the youth employment challenge by providing On-the-job Sales & Marketing Training and Job placement service to equip individuals with necessary sales and marketing skills, enabling them to secure employment and helping business owners find highly skilled salespeople to boost their sales volumes.
           </p>
-          <p className="body-1 max-w-3xl mx-auto mb-1 text-n-2 lg:mb-3 text-center">This is also an online platform which promotes products and services to potential customers, increasing awareness and driving sales. 
+          <p className="body-1 max-w-3xl mx-auto mb-1 text-n-2 lg:mb-3 text-left">This is also an online platform which promotes products and services to potential customers, increasing awareness and driving sales. 
           </p></div>
         </Section>
       </div>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -16,7 +16,7 @@ const Section = ({
       relative 
       ${
         customPaddings ||
-        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+        `py-5 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
       } 
       ${className || ""}`}
     >
